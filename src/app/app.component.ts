0f25886412ecae89c4122934ca33a940f32784db
sunny <mar120434@yahoo.com.tw>
2024-01-21 22:08:41 +0800

    docs: add my todo list 1st. version
    1. app.component.ts 包含 addItem, removeItem, toggleItem 功能，控制 itemList
    
    2. app.component.html 渲染 itemList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'toDoListAngular';
+  itemList = [
+    { name: 'Hit the gym', checked: false, id: 1 },
+    { name: 'Pay bills', checked: true, id: 2 },
+    { name: 'Meet George', checked: false, id: 3 },
+    { name: 'Buy eggs', checked: false, id: 4 },
+    { name: 'Read a book', checked: false, id: 5 },
+    { name: 'Organize office', checked: false, id: 6 }
+  ];
+  newItem = { name: "" };
+
+  toggleItem(itemToggle: any){
+    itemToggle.checked = !itemToggle.checked;
+  }
+
+  removeItem(itemRemove: any){
+    this.itemList = this.itemList.filter(t => t.id !== itemRemove.id);
+  }
+
+  addItem(itemNewName: string){
+    let newId = 0;
+    for (let idx in this.itemList) {
+      if (this.itemList[idx].id >= newId) {
+        newId += this.itemList[idx].id;
+      }
+    }
+    if (itemNewName !== "") {
+      this.itemList.push({ name: itemNewName, checked: false, id: newId });
+    }
+    this.newItem.name = "";
+  }
 }
